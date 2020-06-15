6088d21b0dcdc8ba6b81bbd355cfaff121873c14
auganm17 <35291448+auganm17@users.noreply.github.com>
2020-06-15 12:41:21 +0200

    Add files via upload

--- Java_auganm17_3ahme/src/ue05/Koerper.java
+++ Java_auganm17_3ahme/src/ue05/Koerper.java
@@ -1,6 +1,35 @@
-
 package ue05;
 
-public class Koerper {
+public abstract class Koerper {
     
+    public static final double DICHTE_EICHE = 670;
+    public static final double DICHTE_BUCHE = 690;
+    
+    protected double dichte;
+
+
+    public Koerper (double dichte) throws IllegalArgumentException {
+        if (dichte < 0) {
+            throw new IllegalArgumentException("Invalid parameter dichte");
+        }
+        this.dichte = dichte;
+    }
+
+
+    public double getDichte () {
+        return dichte;
+    }
+
+
+    public void setDichte (double dichte) {
+        this.dichte = dichte;
+    }
+    
+    public abstract double oberflaeche ();
+    
+    public abstract double volumen ();
+        
+    public double masse () {
+        return volumen() * dichte;
+    }      
 }
--- Java_auganm17_3ahme/src/ue05/Kugel.java
+++ Java_auganm17_3ahme/src/ue05/Kugel.java
@@ -1,6 +1,44 @@
-
 package ue05;
 
-public class Kugel {
-    
+import java.util.Locale;
+
+public class Kugel extends Koerper {
+    private double r;
+
+    public Kugel (double r) {
+        super(0.0);
+        if(r <= 0) {
+            throw new IllegalArgumentException("invalid parameter r");
+        }
+        this.r = r;
+    }
+       
+    public Kugel(double r, double dichte) {
+        super(dichte);
+        this.r = r;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+   
+
+    @Override
+    public double oberflaeche () {
+       return 4 * Math.PI * r * r; 
+    }
+
+    @Override
+    public double volumen () {
+        return (4/3) * Math.PI * r * r * r;
+    }
+
+    @Override
+    public String toString () {
+        return String.format(Locale.ENGLISH, "{\"r\":%e\"dichte\":%e", r, dichte);
+    }  
 }
